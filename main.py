616a3322ee1f7c087f8be350e7b38fcd695d0970
Mr. Monkey <mcryry06@gmail.com>
2023-01-10 12:50:18 -0500

    notes

--- main.py
+++ main.py
@@ -4,3 +4,18 @@ for letter in "supercalifragilisticexpialidocious":
       continue
     dumb_word += letter
     print(dumb_word)
+
+name = input("What is your name? ")
+print(name)
+#print(type(name))
+
+age = input("What is your age? ")
+print(age)
+"""
+print(type(age))
+"""
+
+print("3" * 3)
+print(type(3/3))
+print(4 ** 3)
+
