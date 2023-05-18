bc9d3fb6ef6ccd95db74139922a1a32f419ef0e9
Mr. Monkey <mcryry06@gmail.com>
2023-05-18 11:42:10 -0400

    Fixed print log and info functions

--- Final Python Project/translator.py
+++ Final Python Project/translator.py
@@ -1,7 +1,7 @@
 # This program acts as a translator to eliminate language barriers
 # It's function is to automatically detect, and translate diologue
 # I am using an unofficial version of the google translate api for this through a library
-# It will only work in the terminal at the moment but expansion is always an option.
+# User 1 must speak english for now
 
 # https://www.thepythoncode.com/article/translate-text-in-python
 # https://hse.instructure.com/courses/133681/assignments/1271350
@@ -9,13 +9,10 @@
 import json
 import random
 from googletrans import Translator, constants
-from pprint import pprint
 translator = Translator()
 autr = False
 
 # Set variables
-count = 0
-autrlog = []
 user_index = {
     "users": [
         {"name": "User 1", "language": "english"},
@@ -34,7 +31,7 @@ def user_interface():
             print("\n________ MENU ________")
             print("1: Translate")
             print("2: Auto-Translate")
-            print("3: Get Speaker Info")
+            print("3: Info")
             print("4: Print Conversation")
             print("0: Quit Translator\n")
 
@@ -60,23 +57,57 @@ def user_interface():
         elif autr == True:
             autoTranslate()
 
+# Convert ISO-639 code to language
+def convert(ln):
+    with open('Final Python Project/supported_languages.json') as fp:
+        language_list = json.load(fp)
+
+    ll = language_list["languages"]
+    for i in ll:
+        l = i["language"]
+        c = i["code"]
+
+        if c == ln:
+            return l
+
 # Translate a single basic string
 def translate():
-    ln1 = input("Your language: ")
-    ln2 = input("Language to translate to: ")
-    text = input("What do you want to translate? ")
+    u = user_index['users']
+    
+    ln = u[0]["language"]
 
-    translation = translator.translate(text, src=ln1, dest=ln2)
+    if ln == "":
+        ln1 = (f"Language to translate: ")
+    else:
+        print(f"Your current language is {ln}.")
+        ln1 = input(f"If you would like to use that don't enter anything, if not enter the language to translate or \"detect\" for language detection: ").lower()
+        if ln1 == "":
+            ln1 = ln
+            print("Your current language will be used.")
+        elif ln1 == "detect":
+            print("The language will be detected.")
+    ln2 = input("Language to translate to: ")
+    text = input("Text to translate: ")
+
+    if ln1 == "detect":
+        ln = translator.detect(text)
+        print(f"The language {convert(ln.lang)} was detected with a {ln.confidence * 100}% confidence.")
+        ln1 = ln.lang
+    try:
+        translation = translator.translate(text, src=ln1, dest=ln2)
+    except UnboundLocalError:
+        print("There was an error running your translation. This is probably because one or more of your languages is invalid.\nTo change your language or to find a list of valid languages you can use Info in the menu.")
+        return
+    except ValueError:
+        print("There was an error running your translation. This is probably because one or more of your languages is invalid.\nTo change your language or to find a list of valid languages you can use Info in the menu.")
+        return
     tr = f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})"
     print(tr)
 
 # Create messaging platform in terminal for Auto-Translate
 def autoTranslate():
     global autr
-    global count
     global user_index
-
-    count = count + 1
 
     # Read user data
     u = user_index['users']
@@ -123,12 +154,12 @@ def autoTranslate():
                 user_index = {
                     "users": [
                         {"name": "User 1", "language": "english"},
-                        {"name": "User 2", "language": ln.lang}
+                        {"name": "User 2", "language": convert(ln.lang).lower()}
                     ]
                 }
 
                 # Tell users which language was detected and how confident 
-                print(f"The language {ln.lang} was detected with a {ln.confidence * 100}% confidence.")
+                print(f"The language {convert(ln.lang)} was detected with a {ln.confidence * 100}% confidence.")
 
                 # Upload user index to Final Python Project/users.json
                 with open('Final Python Project/users.json', 'w') as json_file:
@@ -158,12 +189,20 @@ def autoTranslate():
                     print("Reminder: If you would like to exit Auto-Translate just say \"exit\".")
                 
                 # Translate text and log it
-                translation = translator.translate(text, src=ln1, dest=ln2)
+                try:
+                    translation = translator.translate(text, src=ln1, dest=ln2)
+                except UnboundLocalError:
+                    print("There was an error running your translation. This is probably because one or more of your languages is invalid.\nTo change your language or to find a list of valid languages you can use Info in the menu.")
+                    autr = False
+                    return
+                except ValueError:
+                    print("There was an error running your translation. This is probably because one or more of your languages is invalid.\nTo change your language or to find a list of valid languages you can use Info in the menu.")
+                    autr = False
+                    return
                 tr = f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})"
-                autrlog.append(tr)
                 print(tr)
             
-            log(autrlog)
+            log(tr)
 
 # Modify user data
 def modify(x):
@@ -252,68 +291,97 @@ def modify(x):
         # Show new modified data
         info()
 
-# Inform and modify user data
+# Display user data or languages
 def info():
     global user_index
 
     with open('Final Python Project/users.json') as fp:
-        users = json.load(fp)
+        user_list = json.load(fp)
+
+    with open('Final Python Project/supported_languages.json') as fp:
+        language_list = json.load(fp)
     
-    u = users['users']
+    u = user_list['users']
+    ll = language_list["languages"]
 
     u1n = u[0]["name"]
     u2n = u[1]["name"]
     u1l = u[0]["language"]
     u2l = u[1]["language"]
 
-    # Display current info if any
-    print("\n++++++++ Info ++++++++\n")
-    # Check for user data
     
-    print("***** User 1 *****")
-    print(f"Name: {u[0]['name']}")
-    print(f"Language: {u[0]['language']}\n")
+    print("\n++++++++ Info ++++++++\n")
+    # Ask the user what they want to know
+    choice = input("What would you like to view? \"users\" for user data \"languages\" for a list of valid languages \"exit\" to exit ").lower()
+    
+    # Check if users was chosen
+    if choice == "users":
+        # Display current info if any
+        print("***** User 1 *****")
+        print(f"Name: {u[0]['name']}")
+        print(f"Language: {u[0]['language']}\n")
 
-    print("***** User 2 *****")
-    print(f"Name: {u[1]['name']}")
-    print(f"Language: {u[1]['language']}\n")
-
-    if u1n != "" and u2n != "" and u1l != "" and u2l != "":
-        # Ask if user would like to modify data
-        if input("would you like to edit this? \"yes\" for yes \"no\" for no ").lower() == "yes":
-            modify(2)
-        else:
-            return
-    else:
-        print("There is no or incomplete user data. You can try out Auto-Translate to automatically add information or you can manually add it.")
-        if input("would you like to manually add the info? \"yes\" for yes \"no\" for no ").lower() == "yes":
-            modify(2)
-        else:
-            return
+        print("***** User 2 *****")
+        print(f"Name: {u[1]['name']}")
+        print(f"Language: {u[1]['language']}\n")
         
+        # Check for complete user data
+        if u1n != "" and u2n != "" and u1l != "" and u2l != "":
+            # Ask if user would like to modify data
+            if input("would you like to edit this? \"yes\" for yes \"no\" for no ").lower() == "yes":
+                modify(2)
+            else:
+                return
+        else:
+            print("There is no or incomplete user data. You can try out Auto-Translate to automatically add information or you can manually add it.")
+            # Ask user if they want to edit the data
+            if input("would you like to manually add the info? \"yes\" for yes \"no\" for no ").lower() == "yes":
+                modify(2)
+            else:
+                return
+    # Check if languages was chosen
+    elif choice == "languages":
+        print("Language                    ISO-639")
+        for i in ll:
+            l = i["language"]
+            c = i["code"]
+            space = " " * (30 - len(l))
+            print(f"{l}{space}{c}")
+
+    # Check if exit was chosen
+    elif choice == "exit":
+        return
+    
+    # If invalid option
+    else:
+        print("Please enter a valid option.")
+        return
 
 # Access the Auto-Translate log
 def log(x):
     # Reset the log
     if x == 1:
-        with open('autrlog.txt', 'w') as out_file:
-                out_file.writelines("")
+        with open('Final Python Project/autrlog.txt', 'w') as out_file:
+            out_file.write("")
 
     # Display the log
-    if x == 2:
-        with open('autrlog.txt', 'r') as in_file:
-            if in_file == "":
-                print("Nothing to log yet. Try out Auto-Translate first and you can read the whole conversation here.")
+    elif x == 2:
+        with open('Final Python Project/autrlog.txt', 'r') as in_file:
+            script = in_file.readlines()
+            if len(script) < 1:
+                print("Nothing to print yet. Try out Auto-Translate first and you can read the whole conversation here.")
             else:
-                print(in_file)
+                for i in script:
+                    print(i)
 
     # Write to the log
     else:
-        with open('autrlog.txt', 'a') as out_file:
-                out_file.write(f"\n{x}")   
+        with open('Final Python Project/autrlog.txt', 'a') as out_file:
+            out_file.write(f"{x}\n")   
 
-# Reset user index and upload to json file for manual modifications to data
+# Reset log and user index and upload to json file for manual modifications to data
 modify(1)
+log(1)
 
 # Run program
 user_interface()
